5a4f2ecacafc9966895b489d07bda2f76de16871
Alex Thompson <Alex@appNerd.com.au>
2022-06-05 19:46:19 +1000

    Initial Commit

---
+++ SwiftUI-Apple-Frameworks/SwiftUI_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksApp.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Alexander Thompson on 5/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
